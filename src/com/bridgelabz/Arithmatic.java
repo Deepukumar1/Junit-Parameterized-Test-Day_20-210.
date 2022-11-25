1c790381f65808f513d926574180b1ad045f68d6
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-25 20:06:31 +0530

    Added: parametrized testing

---
+++ src/com/bridgelabz/Arithmatic.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Arithmatic {
+	 public int sum(int firstNumber,int secondNumber){
+	        int expectedResult=firstNumber+secondNumber;
+	        return expectedResult;
+	    }
+}
